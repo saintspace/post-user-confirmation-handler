ecf8e46297b4809bba42496e7684ef353a74acf7
Jordan Lee Osier <jordan.osier@skillmaple.com>
2023-05-05 07:50:28 -0400

    Post account confirmation event to be handled by worker

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module post-user-confirmation-handler
 
 go 1.20
 
-require github.com/aws/aws-lambda-go v1.40.0 // indirect
+require (
+	github.com/aws/aws-lambda-go v1.40.0 // indirect
+	github.com/aws/aws-sdk-go v1.44.257 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,20 +1,82 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sns"
+	"github.com/aws/aws-sdk-go/service/ssm"
+	"github.com/google/uuid"
 )
 
 func main() {
 	lambda.Start(handlePostConfirmation)
 }
 
+type Event struct {
+	EventName     string `json:"eventType"`
+	CorrelationId string `json:"correlationId"`
+	EventDetails  string `json:"eventDetails"`
+}
+
+type AccountConfirmationTaskEvent struct {
+	UserName     string `json:"userName"`
+	EmailAddress string `json:"emailAddress"`
+}
+
 func handlePostConfirmation(event events.CognitoEventUserPoolsPostConfirmation) (events.CognitoEventUserPoolsPostConfirmation, error) {
-	fmt.Println("Event:", event)
+	userName := event.UserName
 	userEmail := event.Request.UserAttributes["email"]
-	fmt.Println("User email:", userEmail)
+	correlationId := uuid.New().String()
+	accountContirmationTask := AccountConfirmationTaskEvent{
+		UserName:     userName,
+		EmailAddress: userEmail,
+	}
+	accountConfirmationTaskBytes, err := json.Marshal(accountContirmationTask)
+	if err != nil {
+		fmt.Printf("error marshalling account confirmation task for (user name: %v) (error: %v)\n", userName, err.Error())
+	} else {
+		event := Event{
+			EventName:     "account-confirmation-task",
+			CorrelationId: correlationId,
+			EventDetails:  string(accountConfirmationTaskBytes),
+		}
+		eventBytes, err := json.Marshal(event)
+		if err != nil {
+			fmt.Printf("error marshalling account confirmation task event for (user name: %v) (error: %v)\n", userName, err.Error())
+		}
+		eventString := string(eventBytes)
+		awsSession := session.Must(session.NewSessionWithOptions(session.Options{
+			SharedConfigState: session.SharedConfigEnable,
+		}))
+		ssmService := ssm.New(awsSession)
+		paramOutput, err := ssmService.GetParameter(&ssm.GetParameterInput{
+			Name:           aws.String("worker-tasks-topic-arn"),
+			WithDecryption: aws.Bool(false),
+		})
+		if err != nil {
+			fmt.Printf("error getting worker tasks topic arn to send account confirmation task event for (user name: %v) (error: %v)\n", userName, err.Error())
+		} else {
+			topicArn := paramOutput.Parameter.Value
+			snsService := sns.New(awsSession)
+			uniqueMessageId := uuid.New().String()
+			_, err := snsService.Publish(&sns.PublishInput{
+				Message:                &eventString,
+				TopicArn:               topicArn,
+				MessageGroupId:         &uniqueMessageId,
+				MessageDeduplicationId: &uniqueMessageId,
+			})
+			if err != nil {
+				fmt.Printf("error publishing account confirmation task event for (user name: %v) (error: %v)\n", userName, err.Error())
+			} else {
+				fmt.Printf("successfully published account confirmation task event for (user name: %v) (correlationId: %v)\n", userName, correlationId)
+			}
+		}
+	}
 
 	return event, nil
 }
